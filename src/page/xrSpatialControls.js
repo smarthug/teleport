acfd472eda186d013a87e20eba8da9a2c0c893d2
smarthug <kirklayer@gmail.com>
2021-04-14 13:52:37 +0900

    xr setup done

--- src/page/xrSpatialControls.js
+++ src/page/xrSpatialControls.js
@@ -12,12 +12,58 @@ let player, destination;
 
 var room;
 
+let playerPos = new THREE.Vector3();
+let destinationPos = new THREE.Vector3();
+// let cubePos = new THREE.Vector3();
+
+let result = new THREE.Vector3();
+
+let tmp = new THREE.Vector3();
+let tmpQuaternion = new THREE.Quaternion();
+
+
+let cube, box
+
+const controlledObj = {
+    multipliedScalar: 3,
+    teleport: () => { }
+
+}
+
 // var count = 0, amount = 1000;
 // var instancedMesh, objects = [];
 
 // var clock = new THREE.Clock();
 // let  scene, camera, renderer;
 // const clock = new THREE.Clock();
+
+
+
+function TranslateHelperGeometry() {
+
+    const geometry = new THREE.BufferGeometry();
+
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute([0, 0, 0, 1, 1, 1], 3));
+
+    return geometry;
+
+}
+
+
+const matHelper = new THREE.MeshBasicMaterial({
+    depthTest: false,
+    depthWrite: false,
+    transparent: true,
+    side: THREE.DoubleSide,
+    fog: false,
+    toneMapped: false
+});
+
+
+let deltaLine = new THREE.Line(TranslateHelperGeometry(), matHelper);
+
+let deltaLine2 = new THREE.Line(TranslateHelperGeometry(), matHelper)
+
 
 export default function Main() {
     const containerRef = useRef();
@@ -62,12 +108,16 @@ export default function Main() {
             this.userData.isSelecting = true;
             console.log(this)
 
+
+
         }
 
         function onSelectEnd() {
 
             this.userData.isSelecting = false;
             console.log(this)
+
+            Test();
 
             // Test 함수 의 호출을 여기서 ... 
         }
@@ -111,11 +161,34 @@ export default function Main() {
 
         window.addEventListener('resize', onWindowResize, false);
 
-        player = new THREE.Mesh(new THREE.SphereBufferGeometry(0.05, 100, 100), new THREE.MeshNormalMaterial());
+        player = new THREE.Mesh(new THREE.SphereBufferGeometry(0.05, 100, 100), new THREE.MeshStandardMaterial({color:"green"}));
         player.position.set(0, 0.05, 0);
         // controllerGrip1.add(player)
-        controller1.add(player)
+        controller2.add(player)
         // scene.add(player);
+
+        destination = new THREE.Mesh(new THREE.SphereBufferGeometry(0.05, 100, 100), new THREE.MeshStandardMaterial({color:"yellow"}));
+        destination.position.set(0, 0.05, 0);
+        controller1.add(destination);
+
+
+
+        scene.add(deltaLine)
+
+
+
+
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var material = new THREE.MeshNormalMaterial();
+        cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+
+
+        var geometry2 = new THREE.BoxGeometry(1, 1, 1);
+        var material2 = new THREE.MeshNormalMaterial({ wireframe: true });
+        box = new THREE.Mesh(geometry2, material2);
+        scene.add(box);
+
 
     }
 
@@ -153,8 +226,42 @@ export default function Main() {
 
         // var delta = clock.getDelta(); 
 
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        deltaLine.visible = true;
+        // deltaLine2.visible = true
+        player.getWorldPosition(playerPos)
+        destination.getWorldPosition(destinationPos)
+
+        tmp.subVectors(destinationPos, playerPos)
+
+        tmp.multiplyScalar(controlledObj.multipliedScalar);
+        box.position.copy(tmp.add(cube.position))
+
+        player.getWorldQuaternion(tmpQuaternion);
+
+        deltaLine.position.copy(playerPos);
+        tmp.set(1e-10, 1e-10, 1e-10).add(destinationPos).sub(playerPos);
+        deltaLine.scale.copy(tmp);
+
+        deltaLine2.position.copy(cube.position);
+        tmp.set(1e-10, 1e-10, 1e-10).add(cube.position).sub(box.position).multiplyScalar(- 1);
+        deltaLine2.scale.copy(tmp);
+
+
+
+
+
+
         renderer.render(scene, camera);
 
+    }
+
+
+    function Test() {
+        result = tmp.subVectors(destinationPos, playerPos)
+        cube.position.add(result.multiplyScalar(controlledObj.multipliedScalar));
     }
 
 
