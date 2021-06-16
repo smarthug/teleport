8cf7741ad59289fbc07c53453bb23a622f488167
hosuk <kirklayer@gmail.com>
2021-06-16 16:05:59 +0900

    test works

--- example/src/App.js
+++ example/src/App.js
@@ -3,8 +3,14 @@ import React from 'react'
 import { ExampleComponent } from 'three-spatial-controls'
 import 'three-spatial-controls/dist/index.css'
 
+import SpatialControlsPage from './Page/joystickRotateSpatial'
+
+// const App = () => {
+//   return <ExampleComponent text="Create React Library Example 😄" />
+// }
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <SpatialControlsPage />
 }
 
 export default App
